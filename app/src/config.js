724c48be885e0a90c1c74902a1ead899a1e1221e
ifirmawan <firmawaneiwan@gmail.com>
2023-04-14 13:54:24 +0700

    [#2] Change dummy pdf url

--- app/src/config.js
+++ app/src/config.js
@@ -1,4 +1,3 @@
 export const POWER_BI_URL =
   "https://app.powerbi.com/view?r=eyJrIjoiYzRlNmYyM2QtNDE3NC00N2EyLThjZjktYzY4YjIxN2U5ODcwIiwidCI6ImIxNzBlMTE1LWRjM2QtNGU5Mi04NWJlLWU0YjMwMDljNWRjMiIsImMiOjl9";
-export const PDF_URL =
-  "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
+export const PDF_URL = "https://si-wins.org/files/data_explorer_user_guide.pdf";
