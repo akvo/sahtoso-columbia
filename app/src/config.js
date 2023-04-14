0b45bd4582df8c5368cccba071f7f2edc787a5e1
ifirmawan <firmawaneiwan@gmail.com>
2023-04-14 12:42:30 +0700

    [#2] Create a config to store URLs

---
+++ app/src/config.js
@@ -0,0 +1,4 @@
+export const POWER_BI_URL =
+  "https://app.powerbi.com/view?r=eyJrIjoiYzRlNmYyM2QtNDE3NC00N2EyLThjZjktYzY4YjIxN2U5ODcwIiwidCI6ImIxNzBlMTE1LWRjM2QtNGU5Mi04NWJlLWU0YjMwMDljNWRjMiIsImMiOjl9";
+export const PDF_URL =
+  "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
